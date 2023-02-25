217dbda7ceb38d6853e1114434927699b902b774
ksu2002 <simonova278@gmail.com>
2023-02-25 21:07:54 +0500

    added exception

--- src/com/company/Fraction.java
+++ src/com/company/Fraction.java
@@ -1,17 +1,5 @@
 package com.company;
 
-//Описать класс дробей — рациональных чисел, являющихся отношением двух целых
-//чисел. Предусмотреть методы сложения, вычитания, умножения и деления дробей.
-// Разработать класс в соответствии с индивидуальным заданием
-//Каждый разрабатываемый класс должен, как правило, содержать следующие
-//элементы: скрытые поля, конструкторы с параметрами и без параметров,
-
-//методы, свойства. Методы и свойства должны обеспечивать непротиворечивый,
-//полный, минимальный и удобный интерфейс класса. При возникновении ошибок
-//должны выбрасываться исключения.
-//Написать программу, демонстрирующую все разработанные элементы
-//класса. В программе должна выполняться проверка всех разработанных
-//элементов класса
 public class Fraction {
     private int numerator;//числитель
     private int denominator;//знаменатель
@@ -19,20 +7,27 @@ public class Fraction {
     public int getNumerator() {
         return numerator;
     }
+
     public int getDenominator() {
         return denominator;
     }
-    public void print(){
-        System.out.println(numerator+ "/" + denominator);
+
+    public void print() {
+        System.out.println(numerator + "/" + denominator);
     }
+
     public Fraction() {
         this.denominator = 1;
         this.numerator = 1;
     }
 
     public Fraction(int numerator, int denominator) {
-        this.denominator = denominator;
         this.numerator = numerator;
+        if (denominator == 0) {
+            throw new ArithmeticException("Ошибка! Деление на 0");
+        } else {
+            this.denominator = denominator;
+        }
     }
 
     public Fraction add(Fraction fraction) {//сложение дробей
@@ -75,6 +70,8 @@ public class Fraction {
     public Fraction divide(Fraction fraction) {//деление дробей
         int resultNumerator;
         int resultDenominator;
+        if (fraction.numerator == 0)
+            throw new ArithmeticException("Ошибка! Деление на 0");
         resultDenominator = this.denominator * fraction.numerator;
         resultNumerator = this.numerator * fraction.denominator;
         Fraction result = new Fraction(resultNumerator, resultDenominator);
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,30 @@
 package com.company;
-
-import java.util.Scanner;
-
 public class Main {
-    public static int count;
-
     public static void main(String[] args) {
         try {
-            Fraction fraction1 = new Fraction(2,0);
-            if(fraction1.getDenominator()==0){
-                throw new Exception("error");
-            }
-            Fraction fraction2 = new Fraction(4, 6);
-            if(fraction2.getDenominator()==0) {
-                throw new Exception("е");
-            }
+            Fraction fraction1 = new Fraction(2, 3);
+            Fraction fraction2 = new Fraction(3, 5);
+            System.out.print("Первая дробь: ");
             fraction1.print();
+            System.out.print("Вторая дробь: ");
             fraction2.print();
             Fraction fraction3 = new Fraction();
             fraction3 = fraction1.add(fraction2);
+            System.out.print("Результат сложения: ");
             fraction3.print();
-            fraction3 = fraction1.divide(fraction2);
-            fraction3.print();
-            fraction3 = fraction1.multiply(fraction2);
-            fraction3.print();
+            System.out.print("Результат вычитания: ");
             fraction3 = fraction1.subtract(fraction2);
             fraction3.print();
-
-        }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.print("Результат деления: ");
+            fraction3 = fraction1.divide(fraction2);
+            fraction3.print();
+            System.out.print("Результат умножения: ");
+            fraction3 = fraction1.multiply(fraction2);
+            fraction3.print();
+            Fraction fraction4 = new Fraction(5, 0);
+            fraction4.print();
+        } catch (ArithmeticException ae) {
+            System.out.println(ae.getMessage());
         }
 
     }
